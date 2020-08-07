9c9266c116d6fb3180edd08bdd2821935c25496f
vitor7001 <vitorgomes7001@gmail.com>
2020-08-07 11:55:55 -0300

    Relacao entre message e protocol

--- src/config/database/migrations/20200807110755_create_message.js
+++ src/config/database/migrations/20200807110755_create_message.js
@@ -10,6 +10,7 @@ exports.up = knex => knex.schema.createTable('message', table =>{
   table.string('callack_option').default('https://mszenvia.digitalk.com.br/api/v1/incoming/status')
   table.boolean('flash_sms').default(false)
   table.timestamps(true, true)
+  table.foreign('id_protocol').references('protocol.id')
 })
 
 exports.down = function(knex, Promise) {
