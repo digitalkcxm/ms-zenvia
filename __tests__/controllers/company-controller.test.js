f25dddbd35a909287b929d9adc4630c952afcd02
vitor7001 <vitorgomes7001@gmail.com>
2020-08-18 15:15:33 -0300

    testes company

--- __tests__/controllers/company-controller.test.js
+++ __tests__/controllers/company-controller.test.js
@@ -1,33 +1,60 @@
-const CompanyController = require('../../src/controllers/CompanyController')
-const CompanyModel = require('../../src/models/CompanyModel')
 
 const request = require('supertest')
 const app = require('../../src/config/server')
 
-const makeSut = () => {
-  const companyModel = new CompanyModel()
-  const companyController = new CompanyController()
-  return {
-    companyModel, companyController
-  }
-}
+let createdCompany, updatedCompany = {}
 
 describe('Company Controller test', () => {
   it('Should create a new Company', async (done) => {
     const newCompany = {
-      "name": "Test Company1234",
+      "name": "Company2",
       "callback": "https://callback/api/v1/incoming/zenvia"
     }
 
     const result = await request(app)
       .post('/api/v1/company')
       .send(newCompany)
+    createdCompany = result.body
 
     expect(result.id).not.toBeNull()
     expect(result.body.name).toEqual(newCompany.name)
     expect(result.body.callback).toEqual(newCompany.callback)
     expect(result.status).toBe(201)
     expect(result.body.activated).toBeTruthy()
+    done()
+  })
+
+  it('Should update an company', async (done) =>{
+    updatedCompany.id = createdCompany.id
+    updatedCompany.name = 'Updated_name'
+    updatedCompany.callback = 'https://update.com.br'
+    const result = await request(app)
+      .put(`/api/v1/company`)
+      .send(updatedCompany)
+    console.log('ERRO AQUI ==>>', result.error)
+      expect(result.id).not.toBeNull()
+      expect(result.body.name).toEqual(updatedCompany.name)
+      expect(result.body.callback).toEqual(updatedCompany.callback)
+      expect(result.status).toBe(200)
+      expect(result.body.activated).toBeTruthy()
+      done()
+  })
+
+  it('Should get an company by id', async (done) =>{
+    const result = await request(app)
+    .get('/api/v1/company/', createdCompany.id)
+    expect(result.status).toBe(200)
+    expect(result.body[0].id).not.toBeNull()
+    expect(result.body[0].name).toEqual(updatedCompany.name)
+    expect(result.body[0].callback).toEqual(updatedCompany.callback)
+    done()
+  })
+
+  it('Should get all companies', async (done) =>{
+
+    const result = await request(app)
+    .get('/api/v1/company')
+    expect(result.status).toBe(200)
     done()
   })
 })
