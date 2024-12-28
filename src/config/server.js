955ae49e5fc084693ce8a4c0a9860a75372c45d7
Marcus Lozano <marcus.lozano@digitalk.com.br>
2024-12-28 15:49:21 -0300

    refactor: remove apm

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,3 @@
-const tracing = require('./elastic-apm')
 const express = require('express')
 const validator = require('express-validator')
 const bodyParser = require('body-parser')
@@ -13,7 +12,7 @@ app.use(bodyParser.urlencoded({limit: '250mb', extended: true}))
 app.use(validator())
 app.use((req, res, next) => next())
 
-routes(app, tracing)
+routes(app)
 
 if(process.env.NODE_ENV !== 'testing'){
   app.listen(process.env.PORT, () => console.log(`Server running in port ${ process.env.PORT }`))
